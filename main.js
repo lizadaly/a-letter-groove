be24674875b0ae7213684b9f03484e12f2f658ef
Liza Daly <lizadaly@gmail.com>
2022-11-04 16:48:04 -0400

    Put up just this page

--- main.js
+++ main.js
@@ -1,65 +1,66 @@
-const worker = Tesseract.createWorker();
+
 
 (async () => {
-  await worker.load()
-  await worker.loadLanguage('eng')
-  await worker.initialize('eng')
 
-  const image = document.querySelector("#page")
-  const {
-    data
-  } = await worker.recognize(image.src)
-  console.log(data)
 
-  const main = document.querySelector('main')
-  const canvas = document.createElement('canvas')
-  canvas.willReadFrequently = true
-  canvas.width = image.width
-  canvas.height = image.height
-  const ctx = canvas.getContext('2d')
-  ctx.drawImage(image, 0, 0)
-  image.style.display = 'none'
-
-  for (const word of data.words) {
+  for (const image of document.querySelectorAll('.page')) {
+    const worker = Tesseract.createWorker()
+    await worker.load()
+    await worker.loadLanguage('eng')
+    await worker.initialize('eng')
     const {
-      bbox
-    } = word
-    ctx.fillStyle = 'white'
+      data
+    } = await worker.recognize(image.src)
+    console.log(data)
 
-    window.requestAnimationFrame(() => {
-      const idata = ctx.getImageData(bbox.x0, bbox.y0, bbox.x1 - bbox.x0, bbox.y1 - bbox.y0);
+    const main = document.querySelector('main')
+    const canvas = document.createElement('canvas')
+    canvas.willReadFrequently = true
+    canvas.width = image.width
+    canvas.height = image.height
+    const ctx = canvas.getContext('2d')
+    ctx.drawImage(image, 0, 0)
+    image.style.display = 'none'
 
-      // Create a throwaway canvas to do data transformations on
-      const c = document.createElement('canvas')
-      c.width = bbox.x1 - bbox.x0
-      c.height = bbox.y1 - bbox.y0
+    for (const word of data.words) {
+      const {
+        bbox
+      } = word
+      ctx.fillStyle = 'white'
+
+      window.requestAnimationFrame(() => {
+        const idata = ctx.getImageData(bbox.x0, bbox.y0, bbox.x1 - bbox.x0, bbox.y1 - bbox.y0);
+
+        // Create a throwaway canvas to do data transformations on
+        const c = document.createElement('canvas')
+        c.width = bbox.x1 - bbox.x0
+        c.height = bbox.y1 - bbox.y0
 
 
-      const cctx = c.getContext('2d')
-      cctx.putImageData(idata, 0, 0)
-      const im = new Image(c.width, c.height)
-      im.src = c.toDataURL()
-      im.onload = () => {
-        const nc = drawRotated(im, 180)
+        const cctx = c.getContext('2d')
+        cctx.putImageData(idata, 0, 0)
+        const im = new Image(c.width, c.height)
+        im.src = c.toDataURL()
+        im.onload = () => {
+          const nc = drawRotated(im, 180)
+          ctx.drawImage(nc, bbox.x0, bbox.y0)
+        }
+        // const outer = document.createElement('canvas')
+        // outer.width = im.width
+        // outer.height = im.height
+        // const octx = outer.getContext('2d')
+
+        // octx.drawImage(im, 0, 0)
+        // main.append(outer)
+      })
+    }
 
 
-        ctx.drawImage(nc, bbox.x0, bbox.y0)
-      }
-      // const outer = document.createElement('canvas')
-      // outer.width = im.width
-      // outer.height = im.height
-      // const octx = outer.getContext('2d')
 
-      // octx.drawImage(im, 0, 0)
-      // main.append(outer)
-    })
+    main.append(canvas)
+
+    await worker.terminate()
   }
-
-
-
-  main.append(canvas)
-
-  await worker.terminate()
 })()
 
 
