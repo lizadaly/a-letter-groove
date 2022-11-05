2a9d5d1e2cff047fc50283b65e6ddb5f4ade0afb
Liza Daly <lizadaly@gmail.com>
2022-11-04 20:48:40 -0400

    Checkpoint

--- main.js
+++ main.js
@@ -1,73 +1,92 @@
+const url = 'https://iiif.lib.harvard.edu/manifests/drs:5735821'
+
+const req = await fetch(url)
+const manifest = await req.json()
+console.log(manifest)
+
+let i = 0
+const main = document.querySelector('main')
 
 
-(async () => {
 
-
-  for (const image of document.querySelectorAll('.page')) {
-    const worker = Tesseract.createWorker()
-    await worker.load()
-    await worker.loadLanguage('eng')
-    await worker.initialize('eng')
+for (const item of manifest.sequences[0].canvases) {
+  if (i > 51 && i < 55) {
+    const imageUrl = item.images[0].resource["@id"]
     const {
-      data
-    } = await worker.recognize(image.src)
-    console.log(data)
+      width,
+      height
+    } = item.images[0].resource
 
     const main = document.querySelector('main')
     const canvas = document.createElement('canvas')
-    canvas.willReadFrequently = true
-    canvas.width = image.width
-    canvas.height = image.height
-    const ctx = canvas.getContext('2d')
-    ctx.drawImage(image, 0, 0)
-    image.style.display = 'none'
+    canvas.width = width
+    canvas.height = height
+    const ctx = canvas.getContext('2d', {willReadFrequently: true})
 
-    for (const word of data.words) {
+    const image = new Image(width, height)
+    image.crossOrigin = `Anonymous`
+    image.src = imageUrl
+    image.onload = async () => {
+
+
+      const worker = Tesseract.createWorker()
+      await worker.load()
+      await worker.loadLanguage('eng')
+      await worker.initialize('eng')
+
       const {
-        bbox
-      } = word
-      ctx.fillStyle = 'white'
+        data
+      } = await worker.recognize(imageUrl)
 
-      window.requestAnimationFrame(() => {
-        const idata = ctx.getImageData(bbox.x0, bbox.y0, bbox.x1 - bbox.x0, bbox.y1 - bbox.y0);
+      console.log(data)
 
-        // Create a throwaway canvas to do data transformations on
-        const c = document.createElement('canvas')
-        c.width = bbox.x1 - bbox.x0
-        c.height = bbox.y1 - bbox.y0
+      ctx.drawImage(image, 0, 0)
+      image.style.display = 'none'
+
+      for (const word of data.words) {
+        const {
+          bbox
+        } = word
+        ctx.fillStyle = 'white'
+
+        window.requestAnimationFrame(() => {
+          const idata = ctx.getImageData(bbox.x0, bbox.y0, bbox.x1 - bbox.x0, bbox.y1 - bbox.y0);
+
+          // Create a throwaway canvas to do data transformations on
+          const c = document.createElement('canvas')
+          c.width = bbox.x1 - bbox.x0
+          c.height = bbox.y1 - bbox.y0
 
 
-        const cctx = c.getContext('2d')
-        cctx.putImageData(idata, 0, 0)
-        const im = new Image(c.width, c.height)
-        im.src = c.toDataURL()
-        im.onload = () => {
-          const nc = drawRotated(im, 180)
-          ctx.drawImage(nc, bbox.x0, bbox.y0)
-        }
-        // const outer = document.createElement('canvas')
-        // outer.width = im.width
-        // outer.height = im.height
-        // const octx = outer.getContext('2d')
+          const cctx = c.getContext('2d')
 
-        // octx.drawImage(im, 0, 0)
-        // main.append(outer)
-      })
+          cctx.putImageData(idata, 0, 0)
+          const im = new Image(c.width, c.height)
+          im.src = c.toDataURL()
+          im.onload = () => {
+            const nc = drawRotated(im, 45)
+            ctx.drawImage(nc, bbox.x0, bbox.y0)
+          }
+
+        })
+      }
+      await worker.terminate()
+
     }
 
 
 
     main.append(canvas)
 
-    await worker.terminate()
   }
-})()
+  i++
+}
 
 
 const drawRotated = (image, degrees) => {
   const canvas = document.createElement('canvas')
-  canvas.width = image.width
-  canvas.height = image.height
+  canvas.width = image.width * 2
+  canvas.height = image.height * 2
   const ctx = canvas.getContext('2d')
   ctx.save()
   ctx.translate(canvas.width / 2, canvas.height / 2)
