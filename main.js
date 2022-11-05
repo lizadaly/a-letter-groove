8648cc5123d95c31d013be978946dc5498e66327
Liza Daly <lizadaly@gmail.com>
2022-11-05 09:09:19 -0400

    Add IIIF form

--- main.js
+++ main.js
@@ -1,117 +1,88 @@
-const url = 'https://iiif.lib.harvard.edu/manifests/drs:5735821'
-
-const req = await fetch(url)
-const manifest = await req.json()
-console.log(manifest)
+// const url = 'https://iiif.lib.harvard.edu/manifests/drs:5735821'
 
 
 const main = document.querySelector('main')
-
+const form = main.querySelector('form')
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const form = e.target
+  bookRender(form['url'].value)
+})
 
 let i = 0
-for (const item of manifest.sequences[0].canvases) {
 
-  if (i > 52 && i < 60) {
-    const imageUrl = item.images[0].resource["@id"]
-    const {
-      width,
-      height
-    } = item.images[0].resource
+const bookRender = async (url) => {
+  form.style.display = 'none'
+  const req = await fetch(url)
+  const manifest = await req.json()
+  console.log(manifest)
 
-    const main = document.querySelector('main')
-    const canvas = document.createElement('canvas')
-    canvas.width = width
-    canvas.height = height
-    const ctx = canvas.getContext('2d', {
-      willReadFrequently: true
-    })
+  for (const item of manifest.sequences[0].canvases) {
 
-    const image = new Image(width, height)
-    image.crossOrigin = `Anonymous`
-    image.src = imageUrl
-
-    const render = async () => {
-
-      const worker = Tesseract.createWorker()
-      await worker.load()
-      await worker.loadLanguage('eng')
-      await worker.initialize('eng')
-
+    if (i > 51 && i < 63) {
+      const imageUrl = item.images[0].resource["@id"]
       const {
-        data
-      } = await worker.recognize(imageUrl)
+        width,
+        height
+      } = item.images[0].resource
 
-      ctx.drawImage(image, 0, 0)
+      const main = document.querySelector('main')
+      const canvas = document.createElement('canvas')
+      canvas.width = width
+      canvas.height = height
+      const ctx = canvas.getContext('2d', {
+        willReadFrequently: true
+      })
 
-      for (const word of data.words) {
+      const image = new Image(width, height)
+      image.crossOrigin = `Anonymous`
+      image.src = imageUrl
+
+      const render = async () => {
+
+        const worker = Tesseract.createWorker()
+        await worker.load()
+        await worker.loadLanguage('eng')
+        await worker.initialize('eng')
+
         const {
-          bbox
-        } = word
+          data
+        } = await worker.recognize(imageUrl)
+
+        ctx.drawImage(image, 0, 0)
+
+        for (const word of data.words) {
+          const {
+            bbox
+          } = word
 
 
-        ctx.save()
-        ctx.globalCompositeOperation = 'destination-out'
-        ctx.rect(bbox.x0, bbox.y0, bbox.x1 - bbox.x0, bbox.y1 - bbox.y0)
-        ctx.fill()
-        ctx.restore()
+          ctx.save()
+          ctx.globalCompositeOperation = 'destination-out'
+          ctx.rect(bbox.x0, bbox.y0, bbox.x1 - bbox.x0, bbox.y1 - bbox.y0)
+          ctx.fill()
+          ctx.restore()
 
-        ctx.globalCompositeOperation = 'source-over'
+          ctx.globalCompositeOperation = 'source-over'
 
-        ctx.strokeStyle = 'black'
-        ctx.strokeRect(bbox.x0, bbox.y0, bbox.x1 - bbox.x0, bbox.y1 - bbox.y0)
+          ctx.strokeStyle = 'black'
+          ctx.strokeRect(bbox.x0, bbox.y0, bbox.x1 - bbox.x0, bbox.y1 - bbox.y0)
 
-        // window.requestAnimationFrame(() => {
-        //   const idata = ctx.getImageData(bbox.x0, bbox.y0, bbox.x1 - bbox.x0, bbox.y1 - bbox.y0);
+        }
+        await worker.terminate()
 
-        //   // Create a throwaway canvas to do data transformations on
-        //   const c = document.createElement('canvas')
-        //   c.width = bbox.x1 - bbox.x0
-        //   c.height = bbox.y1 - bbox.y0
-
-
-        //   const cctx = c.getContext('2d')
-
-        //   cctx.putImageData(idata, 0, 0)
-        //   const im = new Image(c.width, c.height)
-        //   im.src = c.toDataURL()
-        //   im.onload = () => {
-        //     const nc = drawRotated(im, 45)
-        //     ctx.drawImage(nc, bbox.x0, bbox.y0)
-        //   }
-
-        // })
       }
-      await worker.terminate()
+      if (i % 2 == 0) {
+        console.log("rendering with cut")
+        image.onload = render
 
+      } else {
+        image.onload = () => ctx.drawImage(image, 0, 0)
 
-    }
-    console.log(i)
-    if (i % 2 != 0) {
-      console.log("rendering with cut")
-      image.onload = render
-
-    }
-    else {
-      image.onload = () => ctx.drawImage(image, 0, 0)
+      }
+      main.append(canvas)
 
     }
-    main.append(canvas)
-
+    i += 1
   }
-  i += 1
-}
-
-
-const drawRotated = (image, degrees) => {
-  const canvas = document.createElement('canvas')
-  canvas.width = image.width * 2
-  canvas.height = image.height * 2
-  const ctx = canvas.getContext('2d')
-  ctx.save()
-  ctx.translate(canvas.width / 2, canvas.height / 2)
-  ctx.rotate(degrees * Math.PI / 180)
-  ctx.drawImage(image, -image.width / 2, -image.height / 2)
-
-  ctx.restore()
-  return canvas
 }
