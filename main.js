74f91e7512680816f4ad4f0a312de90a1a8d8e49
Liza Daly <lizadaly@gmail.com>
2022-11-05 11:36:06 -0400

    Next button

--- main.js
+++ main.js
@@ -1,31 +1,33 @@
+const BATCH_SIZE = 20
+
+let url, lastStart
 
 const main = document.querySelector('main')
-const form = main.querySelector('form')
-form.addEventListener('submit', (e) => {
+main.querySelector('form').addEventListener('submit', (e) => {
   e.preventDefault()
   const form = e.target
-  bookRender(form['url'].value)
+  url = form['url'].value
+  lastStart = 0
+  bookRender(url, 0)
 })
 
 let i = 0
 
-const bookRender = async (url) => {
-  form.style.display = 'none'
+const bookRender = async (url, start) => {
+  document.querySelector('form').style.display = 'none'
   const req = await fetch(url)
   const manifest = await req.json()
   console.log(manifest)
 
-  for (const item of manifest.sequences[0].canvases) {
-    if (i > 100 && i <= 150) {
+  for (const item of manifest.sequences[0].canvases.slice(start, start + BATCH_SIZE)) {
 
-      setTimeout(() => {
+    setTimeout(() => {
       const imageUrl = item.images[0].resource["@id"]
       const {
         width,
         height
       } = item.images[0].resource
 
-      const main = document.querySelector('main')
       const canvas = document.createElement('canvas')
       canvas.width = width
       canvas.height = height
@@ -37,8 +39,8 @@ const bookRender = async (url) => {
       image.crossOrigin = 'Anonymous'
       image.src = imageUrl
 
-      const render = async () => {
-
+      image.addEventListener('load', async () => {
+        console.log("rendering ocr...")
         const worker = Tesseract.createWorker()
         await worker.load()
         await worker.loadLanguage('eng')
@@ -48,54 +50,51 @@ const bookRender = async (url) => {
           data
         } = await worker.recognize(imageUrl)
 
+        worker.terminate()
 
         // Only draw the image if there are at least some OCR detections
-        if (data.words.length < 200) {
-          canvas.parentNode.removeChild(canvas)
-          return
+        if (data.words.length > 200) {
+
+          ctx.drawImage(image, 0, 0)
+
+          for (const word of data.words) {
+            const {
+              bbox
+            } = word
+
+            const boxwidth = bbox.x1 - bbox.x0
+            const boxheight = bbox.y1 - bbox.y0
+
+            ctx.save()
+            ctx.globalCompositeOperation = 'destination-out'
+            ctx.rect(bbox.x0, bbox.y0, boxwidth, boxheight)
+            ctx.fill()
+            ctx.restore()
+
+            ctx.globalCompositeOperation = 'source-over'
+            ctx.strokeStyle = 'black'
+            ctx.strokeRect(bbox.x0, bbox.y0, boxwidth, boxheight)
+          }
+          main.insertBefore(canvas, main.firstChild)
+          document.querySelector('button').classList.remove('hidden')
         }
-        ctx.drawImage(image, 0, 0)
+      })
 
-        for (const word of data.words) {
-          const {
-            bbox
-          } = word
+    }, 300)
 
-          const boxwidth = bbox.x1 - bbox.x0
-          const boxheight = bbox.y1 - bbox.y0
-          const boxarea = boxwidth * boxheight
-
-          ctx.save()
-          ctx.globalCompositeOperation = 'destination-out'
-          ctx.rect(bbox.x0, bbox.y0, boxwidth, boxheight)
-          ctx.fill()
-          ctx.restore()
-
-          ctx.globalCompositeOperation = 'source-over'
-          ctx.strokeStyle = 'black'
-          ctx.strokeRect(bbox.x0, bbox.y0, boxwidth, boxheight)
-
-        }
-        await worker.terminate()
-
-      }
-      image.onload = render
-
-      // if (i % 2 == 0) {
-      //   image.onload = render
-
-      // } else {
-      //   image.onload = () => ctx.drawImage(image, 0, 0)
-      // }
-      main.append(canvas)
-
-      }, 300)
-    }
     i += 1
-    document.querySelector('button').classList.remove('hidden')
   }
-  document.querySelector('button').addEventListener('click', () => {
-    const canvas = main.querySelector('canvas:last-of-type')
-    canvas.parentNode.removeChild(canvas)
-  })
 }
+
+document.querySelector('button').addEventListener('click', () => {
+  const canvas = main.querySelector('canvas:last-of-type')
+  canvas.parentNode.removeChild(canvas)
+  console.log([...main.querySelectorAll('canvas')].length)
+
+  if ([...main.querySelectorAll('canvas')].length < 5) {
+    lastStart = lastStart + BATCH_SIZE
+
+    console.log(`Triggering new batch starting from ${lastStart}`)
+    bookRender(url, lastStart)
+  }
+})
